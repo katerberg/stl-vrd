e812d618aef87f4815335dba5783015dcff83976
Mark Katerberg <katerberg@fastmail.fm>
2019-06-01 23:08:31 -0500

    Adds basic routing

---
+++ src/pages/players/naveen-balaji.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function NaveenBalaji() {
+  return (
+    <div className="naveen-balaji">
+      <h1>Naveen Balaji</h1>
+    </div>
+  );
+}
+
+export default NaveenBalaji;
+
---
+++ src/routes.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
+import Naveen from './pages/players/naveen-balaji';
+import Home from './pages/home';
+
+function Routes() {
+
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/player/naveen" exact component={Naveen} />
+        <Redirect to="/" />
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default Routes;
