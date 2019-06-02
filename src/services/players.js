ea60d8bc47638cedf4cfcdbed26b02dff05f19ab
Mark Katerberg <katerberg@fastmail.fm>
2019-06-02 17:49:10 -0500

    Adds a few more players and figures out spacing

--- src/services/players.js
+++ src/services/players.js
@@ -2,6 +2,10 @@ import naveenLarge from '../static/pictures/naveen-balaji.jpg';
 import naveenSmall from '../static/pictures/naveen-balaji-small.jpg';
 import jeffLarge from '../static/pictures/jeff-blyden.jpg';
 import jeffSmall from '../static/pictures/jeff-blyden-small.jpg';
+import alexLarge from '../static/pictures/alex-werth.jpg';
+import alexSmall from '../static/pictures/alex-werth-small.jpg';
+import stephenLarge from '../static/pictures/stephen-hagan.jpg';
+import stephenSmall from '../static/pictures/stephen-hagan-small.jpg';
 
 const naveenbalaji = {
   firstName: 'Naveen',
@@ -23,7 +27,32 @@ const jeffblyden = {
   previousDeck: 'Reanimator',
 };
 
-export const getPlayers = () => ({naveenbalaji, jeffblyden});
+const alexwerth = {
+  firstName: 'Alex',
+  lastName: 'Werth',
+  picture: alexLarge,
+  thumbnail: alexSmall,
+  competitiveDeck: 'Pauper Monarch',
+  edhDeck: 'Queen Marchesa',
+  previousDeck: 'Mono G Ramp',
+};
+
+const stephenhagan = {
+  firstName: 'Stephen',
+  lastName: 'Hagan',
+  picture: stephenLarge,
+  thumbnail: stephenSmall,
+  competitiveDeck: '',
+  edhDeck: '',
+  previousDeck: 'N/A',
+};
+
+export const getPlayers = () => ({
+  naveenbalaji,
+  jeffblyden,
+  alexwerth,
+  stephenhagan,
+});
 
 export default {
   getPlayers,
