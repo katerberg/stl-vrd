545513f09d96eefa3e3057dc41e3941bfe0b04d8
Mark Katerberg <katerberg@fastmail.fm>
2019-06-03 10:49:22 -0500

    Home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import './home.css';
 
 function Home() {
   return (
     <div className="home">
-      <h1>Home</h1>
+      <h1>STL</h1>
+      <h1>VRD</h1>
     </div>
   );
 }
